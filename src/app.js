969fcea56f925c6da59cccee86f3dee57fa7ea7c
John Angel Henao <john.alvarez@correounivalle.edu.co>
2025-02-19 13:56:20 -0500

    Initial commit

--- src/app.js
+++ src/app.js
@@ -9,17 +9,16 @@ const app = express();
 // Configurar CORS
 const corsOptions = {
   origin: '*', // Dirección de tu frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos 
   allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
 };
 app.use(cors(corsOptions)); // Agregar middleware de CORS
-
+ 
 // Middleware de Morgan para registrar las peticiones
-app.use(morgan('dev'));
-app.use(express.json());
-
+app.use(morgan('dev'));    
+app.use(express.json()); 
 // Ruta para subir productos
-app.post('/api/products', createProduct);
+app.post('/api/products', createProduct); 
 app.get('/api/products', getProducts);
 
 // Rutas de autenticación
