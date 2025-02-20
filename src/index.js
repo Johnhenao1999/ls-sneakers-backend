07cc1c29158e5da15572c7743aa1296c5d666efd
John Angel Henao <john.alvarez@correounivalle.edu.co>
2025-02-20 02:15:43 -0500

    Ajuste vercel prod

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-import app from "./app.js"
-import { connectDB } from "./db.js"
+import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import authRoutes from './routes/auth.routes.js';
 
-connectDB()
-app.listen(3000)
-console.log('Server on port', 3000)
+const app = express();
+
+// Configurar CORS
+const corsOptions = {
+  origin: ['http://localhost:5174', 'https://lsneakers-backend.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api', authRoutes);
+
+// Ruta raíz
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel!');
+});
+
+// Conexión a MongoDB antes de exportar `app`
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://lsneakers:r3DcgZw42iE6t2Ef@lsneakers.pg47i.mongodb.net/?retryWrites=true&w=majority&appName=lsneakers', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ Connected to MongoDB');
+  } catch (error) {
+    console.error('❌ MongoDB Connection Error:', error);
+  }
+};
+
+// Conéctate a MongoDB antes de exportar `app`
+await connectDB();
 
 export default app;
+ 
